51079e6acba1820f9e7fe7f71fc8d668a42bc458
Alex Hilton <dragonhilton@gmail.com>
2014-11-17 23:06:50 +0800

    Add project NotTomorrow
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ NotTomorrow/NotTomorrow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotTomorrow
+//
+//  Created by Alex Hilton on 11/17/14.
+//  Copyright (c) 2014 Alex Hilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
